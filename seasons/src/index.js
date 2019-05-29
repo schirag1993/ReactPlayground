ac088a56909daa34e1b7d15b701173425a9841bb
S <schirag1993@users.noreply.github.com>
2019-05-29 21:06:12 +0530

    modify season app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {lat: null, errorMessage: ""}; // only time we directly assign a value to state obj
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {lat: null, errorMessage: ""}; // only time we directly assign a value to state obj
+  // }
+  state = {lat: null, errorMessage: ""};
+  componentDidMount() {
+    console.log("Component mounted!");
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat: position.coords.latitude});
-        console.log(this.state);
-    }, (err) => {
-      this.setState({errorMessage: err.message})
-      console.log(this.state);
-    }
+      (position) => this.setState({lat: position.coords.latitude}),
+        (err) => this.setState({errorMessage: err.message})
     );
+  }
+  componentDidUpdate() {
+    console.log("Component updated!");
   }
 
     render() {
       if(!this.state.lat && !this.state.errorMessage) return <div>Loading...</div>
-      if(this.state.lat) return <div> Latitude: {this.state.lat}</div>
+      if(this.state.lat) return <SeasonDisplay lat={this.state.lat}/>
       return <div> Error: {this.state.errorMessage}</div>
     }
 
