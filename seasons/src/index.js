bea0017586325d2fad93c4d3cfc70f16e95164ce
S <schirag1993@users.noreply.github.com>
2019-05-29 19:57:37 +0530

    learn state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,22 +4,35 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {lat: null}; // only time we direct;y assign a value to state obj
+    this.state = {lat: null, errorMessage: ""}; // only time we directly assign a value to state obj
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({lat: position.coords.latitude})
+        this.setState({lat: position.coords.latitude});
         console.log(this.state);
     }, (err) => {
-      console.log(err.message);
+      this.setState({errorMessage: err.message})
+      console.log(this.state);
     }
     );
   }
 
-  render() {
+    render() {
+      if(!this.state.lat && !this.state.errorMessage) return <div>Loading...</div>
+      if(this.state.lat) return <div> Latitude: {this.state.lat}</div>
+      return <div> Error: {this.state.errorMessage}</div>
+    }
 
-    return <div> Latitude: {this.state.lat}</div>
-  }
+  // render() {
+  //     return (
+  //       <div> Latitude: {this.state.lat}
+  //       <br/>
+  //       Error: {this.state.errorMessage}
+  //       </div>
+  //     )
+  //   }
 }
+
+
 
 
 
