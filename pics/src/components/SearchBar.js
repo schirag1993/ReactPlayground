c3e55e27fb4712b4a292b261b3ff78ba2ad9ffa4
S <schirag1993@users.noreply.github.com>
2019-06-02 19:20:33 +0530

    add controlled unit

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  state = {term: ''};
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+  // onInputClick(event) {
+  //   console.log("Input was clicked");
+  //   console.log(event);
+  // }
+
   render() {
     return (
       <div className="ui segment">
       <form className ="ui form">
       <div className="field">
       <label>Image Search</label>
-      <input type='text' />
+      <input type='text' value={this.state.term} onChange={(e) => this.setState({term: e.target.value}) /> {/* onChange is a special class; onClick={this.onInputClick}; refactor: <input type='text' onChange={(e) => console.log(e.target.value)} /> */}
       </div>
       </form>
       </div>
